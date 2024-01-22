c08c29a97492fdc569c22738aa0aab872b3cf65f
fockjef <git@fockjef.net>
2024-01-22 17:56:19 -0600

    Makes stuff more better or something

--- http_auth.js
+++ http_auth.js
@@ -1,17 +1,28 @@
-// strings http-auth.cap | grep -i user | grep -i pass
-// name=superuser&pass=thisistotallysecure&openid_identifier=&op=Log+in&remember_me=1& ...
-
 (function( challenge_id = "http_auth" ){
 
-	runSolution( { challenge_id, solution } );
+    runSolution( { challenge_id, solution } );
 
-	function solution( tag ){
-		let user = "superuser",
-		    pass = "thisistotallysecure";
-		console.info( user );
-		console.info( pass );
-		tag.refs.username.value = user;
-		tag.refs.password.value = pass;
-		tag.submitAnswer();
-	}
+    async function solution( tag ){
+        let downloadPCAP = true,
+            user, pass;
+
+        if( downloadPCAP ){
+            let url = HackerChallenge.getBinaryLink( `${challenge_id}/http-auth.pcap` )[0].href,
+                data = await fetch(url).then(res => res.text());
+            user = data.match(/(?<=name=)[^&]+/)[0];
+            pass = data.match(/(?<=pass=)[^&]+/)[0];
+        }
+        else{
+            // strings http-auth.cap | grep -i name | grep -i pass
+            // name=superuser&pass=thisistotallysecure&openid_identifier=&op=Log+in&remember_me=1& ...
+            user = "superuser",
+            pass = "thisistotallysecure";
+        }
+
+        console.info( user );
+        console.info( pass );
+        tag.refs.username.value = user;
+        tag.refs.password.value = pass;
+        tag.submitAnswer();
+    }
 })();
