47e1fea0c87af2e7fded71e60a07c06f86f15718
fockjef <git@fockjef.net>
2024-01-22 17:33:36 -0600

    Waits for challenge card to load.

--- disabled_button.js
+++ disabled_button.js
@@ -2,8 +2,15 @@
 
 	runSolution( { challenge_id, solution } );
 
-	function solution( tag ){
+	async function solution( tag ){
+		while( tag.refs.submitButton.disabled == false ){
+			await sleep(100);
+		}
 		tag.refs.submitButton.disabled = false;
 		tag.submit( {} );
+	}
+
+	function sleep( ms ){
+		return new Promise( resolve => setTimeout( resolve, ms ) );
 	}
 })();
