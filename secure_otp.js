fea4cbfd2504f6fd8720ad3d30f2201f9854f43b
fockjef <git@fockjef.net>
2024-01-22 19:57:56 -0600

    Again

--- secure_otp.js
+++ secure_otp.js
@@ -3,19 +3,19 @@
     runSolution( { challenge_id, solution } );
 
     async function solution( tag ){
-        console.log(tag.refs.challengeContent);
+        console.log(tag.refs.challengeContent.innerText);
         await sleep(250);
-        console.log(tag.refs.challengeContent);
+        console.log(tag.refs.challengeContent.innerText);
         await sleep(250);
-        console.log(tag.refs.challengeContent);
+        console.log(tag.refs.challengeContent.innerText);
         await sleep(250);
-        console.log(tag.refs.challengeContent);
+        console.log(tag.refs.challengeContent.innerText);
         await sleep(250);
-        console.log(tag.refs.challengeContent);
+        console.log(tag.refs.challengeContent.innerText);
         await sleep(250);
-        console.log(tag.refs.challengeContent);
+        console.log(tag.refs.challengeContent.innerText);
         await sleep(250);
-        console.log(tag.refs.challengeContent);
+        console.log(tag.refs.challengeContent.innerText);
     }
     function sleep( ms ){
         return new Promise( resolve => setTimeout( resolve, ms ) );
