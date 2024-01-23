4068c7ccf4fc1cad30495a25c1abde72c9ed3385
fockjef <git@fockjef.net>
2024-01-22 18:31:12 -0600

    Fixes padding.

--- birthday.js
+++ birthday.js
@@ -6,7 +6,7 @@
         for(let y = 1900; y <= 2024; y++){
             for(let m = 1; m <= 12; m++){
                 for(let d = 1; d <= 31; d++){
-                    let date = `${m.padStart(2, "0")}/${d.padStart(2, "0")}/${y}`;
+                    let date = `${m.toString().padStart(2, "0")}/${d.toString().padStart(2, "0")}/${y}`;
                     if( sha256(date) == tag.challenge.date_hash ){
                         Birthday_get_today = () => date;
                         tag.submit({});
