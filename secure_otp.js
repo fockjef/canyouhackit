ab90a9c767aaae2727f361515b6312947a065369
fockjef <git@fockjef.net>
2024-01-22 19:56:24 -0600

    More secure_otp testing

--- secure_otp.js
+++ secure_otp.js
@@ -3,6 +3,21 @@
     runSolution( { challenge_id, solution } );
 
     async function solution( tag ){
-        console.log("seed", tag.challenge.seed);
+        console.log(tag.refs.challengeContent);
+        await sleep(250);
+        console.log(tag.refs.challengeContent);
+        await sleep(250);
+        console.log(tag.refs.challengeContent);
+        await sleep(250);
+        console.log(tag.refs.challengeContent);
+        await sleep(250);
+        console.log(tag.refs.challengeContent);
+        await sleep(250);
+        console.log(tag.refs.challengeContent);
+        await sleep(250);
+        console.log(tag.refs.challengeContent);
+    }
+    function sleep( ms ){
+        return new Promise( resolve => setTimeout( resolve, ms ) );
     }
 })();
