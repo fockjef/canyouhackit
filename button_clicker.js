d88da6a7262833f102af1937d64856c87be01334
fockjef <git@fockjef.net>
2024-01-26 13:04:17 -0600

    Actually adds proper ELF parser

--- button_clicker.js
+++ button_clicker.js
@@ -5,6 +5,6 @@
     async function solution( tag ){
         ButtonClicker_num_clicks = 1e6;
         ButtonClicker_click_button({originalEvent:{isTrusted: true}, type: "click"});
-        tag.submitAnswer();
+        tag.submit({});
     }
 })();
